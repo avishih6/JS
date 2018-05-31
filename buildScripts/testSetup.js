3e397a76564b13b0b96ba243ff24760f21ab049d
Avishay Hadar <avishih6@gmail.com>
2018-05-31 13:17:06 +0300

    Building Dev environment in progress

---
+++ buildScripts/testSetup.js
@@ -0,0 +1,6 @@
+// This file isn't transpiled, so must use commonJS & ES5
+
+//Register babel transpile before our test run
+require('babel-register')();
+
+require.extensions['css'] = function() {};
